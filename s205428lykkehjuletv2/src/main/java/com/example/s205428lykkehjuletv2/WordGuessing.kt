94e77e426cd8fff80f6dec52a13b1ef09f23247a
G-morgensol <s205428@student.dtu.dk>
2021-12-02 23:39:16 +0100

    working on user input

--- s205428lykkehjuletv2/src/main/java/com/example/s205428lykkehjuletv2/WordGuessing.kt
+++ s205428lykkehjuletv2/src/main/java/com/example/s205428lykkehjuletv2/WordGuessing.kt
@@ -4,11 +4,13 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
 import com.example.s205428lykkehjuletv2.Model.Player
 import com.example.s205428lykkehjuletv2.databinding.FragmentWordGuessingBinding
 import kotlinx.android.synthetic.main.fragment_word_guessing.*
+import java.util.*
 
 class WordGuessing : Fragment() {
     private var binding: FragmentWordGuessingBinding? = null
@@ -26,10 +28,22 @@ class WordGuessing : Fragment() {
             NavHostFragment.findNavController(this@WordGuessing)
                 .navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
-        val p1 = Player("player")
-        var livesText = "Current lives: " +p1.lives.toString()
-        textview_lives.text = livesText
+        binding!!.buttonGuess.setOnClickListener {
+            guessLetter()
+        }
 
+
+        val p1 = Player("player")
+        val livesText = "Current lives: " +p1.lives.toString()
+        textview_lives.text = livesText
+        //val editText = editTextEnterLetter.editableText
+        //Test
+
+
+
+        }
+    fun guessLetter(){
+        val character = readLine()!![0]
     }
 
     override fun onDestroyView() {
